4c47c4092a2281bee092bb8d60f9670b527bac73
anlogan <anlogan@mines.edu>
2020-06-22 16:17:33 -0600

    RPM Bars and temperatures UI working

--- LCDTemperatureWidget.ino
+++ LCDTemperatureWidget.ino
@@ -10,7 +10,13 @@
 TFT TFTscreen = TFT(cs, dc, rst);
 
 // char array to print percentage to screen
-char valArray[4];
+char tempArray[5];
+char percentArray[5];
+bool flag = true;
+int cpuPercent =43;
+int gpuPercent =100;
+int cpuTemp =0;
+int gpuTemp =0;
 
 class RGBval
 {
@@ -24,19 +30,24 @@ class RGBval
       this->g = g;
       this->b = b;
     }
+    void set_rgb(int r, int g, int b) {
+      this->r = r;
+      this->g = g;
+      this->b = b;
+    }
 };
 
 class Rectangle
 {
   public:
-    int x,y,l,w;
+    int x,y,l,h;
     RGBval rgb;
     Rectangle();
-    Rectangle(int x, int y, int l, int w, RGBval rgb) {
+    Rectangle(int x, int y, int l, int h, RGBval rgb) {
       this->x = x;
       this->y = y;
       this->l = l;
-      this->w = w;
+      this->h = h;
       this->rgb = rgb;
     }
 
@@ -46,38 +57,41 @@ class Rectangle
       this->rgb.b = b;
     }
 };
+
 void setup()
 {
-
   // Put this line at the beginning of every sketch that uses the GLCD:
   TFTscreen.begin();
 
   // clear the screen with a black background
   TFTscreen.background(0, 0, 0);
 
-  // write the static text to the screen
   // set the font color to white
   TFTscreen.stroke(255, 255, 255);
   // ste the font size very large for the loop
   TFTscreen.setTextSize(5);
 }
 
-int a = 0;
-int b = 0;
-int counter = 0;
-bool flag = true;
-int percentage = 0;
+void drawPercent(double percent, RGBval rgb, int x, int y)
+{
+  String percentString = String((int)percent) + "%";
+  percentString.toCharArray(percentArray, 5);
 
+  TFTscreen.setTextSize(2);
 
+  // set the font color
+  TFTscreen.stroke(rgb.b, rgb.g, rgb.r);
+  // print the sensor value
+  TFTscreen.text(percentArray, x, y);
+}
 
 int drawRectangle(Rectangle rect, RGBval rgb)
 {
   TFTscreen.noStroke();
   TFTscreen.fill(rgb.b, rgb.g, rgb.r); // set fill color
-  TFTscreen.rect(rect.x, rect.y, rect.l, rect.w);  
+  TFTscreen.rect(rect.x, rect.y, rect.l, rect.h);  
   return 1;
 }
-
 
 void drawBar(double percent, Rectangle outer, Rectangle inner)
 {
@@ -85,56 +99,72 @@ void drawBar(double percent, Rectangle outer, Rectangle inner)
   // draw outer rectangle
   drawRectangle(outer, outer.rgb);
 
-  // check percent for color?
-  
   // Inner rectangle
-  if (percent > 0 && percent <= 33) {
+  if (percent > 0 && percent <= 50) {
     inner.set_rgb(30,230,30); // green 
   }
-  else if (percent >33 && percent <=66) {
+  else if (percent >50 && percent <=70) {
     inner.set_rgb(255,255,0); // yellow
   }
-  else if (percent >66 && percent <=100) {
+  else if (percent >70 && percent <=100) {
     inner.set_rgb(255,0,0); // red
   }
   else {
     //TODO error over 100%
   }
-  drawRectangle(inner,inner.rgb);
-  
+
   // draw inner w/ percent change
+  double lengthAdj = 84 * percent / (double)100;
+  inner.l = (int)lengthAdj; // Vary this value by % of 84? 
+  drawRectangle(inner,inner.rgb);
+  RGBval percentRGB(30,30,30);
+  // -17 and -10 for x and y are to center the text itself, otherwise it centers the upper left corner
+  drawPercent(percent, percentRGB, outer.l / (2) +outer.x - 17, outer.h /2 + outer.y - 7 );
 }
 
-void drawPercent(double percentVal, RGBval rgb)
+void drawTemp(double tempVal, int x, int y)
 {
-  String percentString = String((int)percentVal) + "%";
-  percentString.toCharArray(valArray, 4);
-
+  String tempString = String((int)tempVal) + "c";
+  tempString.toCharArray(tempArray, 5);
+  RGBval rgb;
   TFTscreen.setTextSize(3);
 
+  if (tempVal > 0 && tempVal <= 50) {
+    rgb.set_rgb(30,230,30); // green 
+  }
+  else if (tempVal >50 && tempVal <=75) {
+    rgb.set_rgb(255,255,0); // yellow
+  }
+  else if (tempVal >75 && tempVal <=100) {
+    rgb.set_rgb(255,0,0); // red
+  }
   // set the font color
   TFTscreen.stroke(rgb.b, rgb.g, rgb.r);
   // print the sensor value
-  TFTscreen.text(valArray, 5, 10);
+  TFTscreen.text(tempArray, x, y);
 }
+
+RGBval outerCPURGB(190,190,190);
+RGBval innerCPURGB(0,240,0);
+RGBval outerGPURGB(190,190,190);
+RGBval innerGPURGB(0,240,0);
+
+Rectangle outerCPU(63,10, TFTscreen.width() - 70,35, outerCPURGB);
+Rectangle innerCPU(66,13, TFTscreen.width() - 76,29, innerCPURGB);
+Rectangle outerGPU(63,65, TFTscreen.width() - 70,35, outerGPURGB);
+Rectangle innerGPU(66,68, TFTscreen.width() - 76,29, innerGPURGB);
 
 void loop()
 {
-  RGBval outerCPURGB(190,190,190);
-  RGBval innerCPURGB(0,240,0);
-  RGBval outerGPURGB(190,190,190);
-  RGBval innerGPURGB(0,240,0);
-  
-  Rectangle outerCPU(60,10, TFTscreen.width() - 70,35, outerCPURGB);
-  Rectangle innerCPU(63,13, TFTscreen.width() - 76,29, outerCPURGB);
-  Rectangle outerGPU(60,65, TFTscreen.width() - 70,35, outerCPURGB);
-  Rectangle innerGPU(63,68, TFTscreen.width() - 76,29, outerCPURGB);
   // draw top bar
-  drawBar(40, outerCPU, innerCPU);
+  drawBar(cpuPercent, outerCPU, innerCPU);
   // draw bot bar
-  drawBar(70, outerGPU, innerGPU);
-  // drawRectangle(outerGPU,outerGPURGB);
-  // drawRectangle(innerGPU,innerGPURGB);
+  drawBar(gpuPercent, outerGPU, innerGPU);
+  // CPU Temp
+  drawTemp(40, 5, 16);
+  // GPU Temp
+  drawTemp(32, 5, 71);
+
 
   // wait for a moment
   delay(2000);
